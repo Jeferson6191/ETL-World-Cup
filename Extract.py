7f4b6a473dfe36f312ad0d686a420af3e617e90d
Jeferson6191 <jeffrodjs0@gmail.com>
2026-06-19 21:07:32 -0300

    baixando arquivos para ser tratados

--- Extract.py
+++ Extract.py
@@ -1,29 +1,10 @@
-from click import option
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait, Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import os
+import kagglehub
 
-#configurações
+# Baixando arquivos
 downloadpath = os.path.abspath("files")
 os.makedirs(downloadpath, exist_ok=True)
-options = Options()
-options.add_argument("--start-maximized")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("useAutomationExtension", False)
-options.add_argument("--disable-notifications")
-prefs = {
-    "download.default_directory": downloadpath,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-}
-options.add_experimental_option("prefs",prefs)
-
-driver = webdriver.Chrome(options=options)
+path = kagglehub.dataset_download(
+    "mominullptr/fifa-world-cup-2026-dataset",
+    output_dir=downloadpath)
+print("Path to dataset files:", path)
